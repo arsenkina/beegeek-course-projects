86cf33f239d7626bd2c8acaaf26bb482c590ca24
Nicole Arsenkina <nicole.arsenkina@gmail.com>
2022-11-20 14:45:51 +0300

    Guessing game was improved

--- guessing_game.py
+++ guessing_game.py
@@ -1,25 +1,64 @@
 from random import randint
 
 MIN_NUM = 1
-MAX_NUM = 100
+
+
+def is_number(s):
+    return s.isdigit()
+
+
+def get_number_with_bounds(max_num):
+    def helper(s):
+        return is_number(s) and MIN_NUM <= int(s) <= max_num
+
+    return helper
+
+
+def get_valid(predicate, text):
+    data = input(text + '\n')
+
+    while not predicate(data):
+        data = input(text + '\n')
+
+    return data
 
 
 def main():
-    num = randint(MIN_NUM, MAX_NUM)
-    print('Рыба карась – игра началась!')
+    max_num = int(get_valid(
+        is_number,
+        'Введите верхнюю границу для случайного выбора числа'))
 
-    while True:
-        predict = int(input())
+    num = randint(MIN_NUM, max_num)
+    attempts = 0
 
-        if predict > num:
-            print('Слишком много, попробуйте еще раз')
+    while True:  # game cycle
+        print('Рыба карась – игра началась!')
 
-        elif predict < num:
-            print('Слишком мало, попробуйте еще раз')
+        while True:  # number choose cycle
+            predict = int(get_valid(
+                get_number_with_bounds(max_num),
+                f'Пожалуйста, введите число от {MIN_NUM} до {max_num}'))
 
-        else:
-            print('Вы угадали, поздравляем!')
-            break
+            if predict > num:
+                print('Слишком много, попробуйте еще раз')
+                attempts += 1
+
+            elif predict < num:
+                print('Слишком мало, попробуйте еще раз')
+                attempts += 1
+
+            else:
+                print(f'Вы угадали, поздравляем! Вам потребовалось {attempts} попыток')
+                attempts = 0
+
+                print('Хотите сыграть ещё раз?')
+                answer = get_valid(lambda ans: ans.lower() == 'да' or ans.lower() == 'нет',
+                                   'Введите \'Да\' или \'Нет\'').lower()
+
+                if answer == 'да':
+                    break
+                else:
+                    return
 
 
 if __name__ == '__main__':
